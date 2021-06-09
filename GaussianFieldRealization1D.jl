7379efa916b706a69175e82ddbccedc829582de2
aloispichler <alois.pi@gmail.com>
2021-06-09 14:42:31 +0200

    Vector types

---
+++ GaussianFieldRealization1D.jl
@@ -0,0 +1,34 @@
+"""
+	display samples of a Gaussian random field
+	created: 2021, May
+    author©: Alois Pichler
+"""
+
+using Gnuplot; Gnuplot.options.gpviewer= true;	# external viewer
+include("KernelsRKHS.jl")
+
+function Kernel(x, y)
+#	MaternKernel(x, y; ℓ= .2, ν= 1.5)
+#	LaplaceKernel(x,y; ℓ= 2.)
+#	GaussKernel(x,y; ℓ= .2)
+	BrownKernel(x,y)
+#	BrownBridge(x,y; ℓ= .2)
+#	SigmoidKernel(x,y; ℓ= .2)
+#	multiquadraticKernel(x,y)
+#	StudentTKernel(x,y)
+end
+
+println("────────────────────── ", time())
+
+copies= 5
+x= range(0, 1, length=1000)
+@gp :- "reset"
+@gp "set terminal wxt 0 title 'Gaussian random field'" :-
+@gp :- "set title 'RKHS realizations'" "set xlabel 'x'" "set border 1" :-
+for i= 1: copies
+	fRKHS= RKHS(collect(x), kernel= Kernel)	#	new realization
+	@gp :- x fRKHS.(x) "w line title 'realization $i' lw ($i<2?3:1)"
+end
+@gp
+
+time()	
--- KernelsRKHS.jl
+++ KernelsRKHS.jl
@@ -1,5 +1,5 @@
 """
-	RKHS struct – reproducing kernel Hilbert space
+	reproducing kernel Hilbert space RKHS – struct definitions
 	created: 2021, May
 	author©: Alois Pichler
 """
@@ -15,11 +15,13 @@ struct RKHS{T} 				# RKHS vector supported by T, describing the RKHS function
 	kernel								# the kernel function
 end
 
-#	│	evaluate the RKHS function f at all x: functor
+#	│	functor to evaluate the RKHS function f at x
 #	╰────────────────────────────────────────────────────
-function (f::RKHS)(x::T) where T	# callable functor
+function (f::RKHS{T})(x::T) where T	# callable functor
+	length(f.x) == length(f.w) || throw(DimensionMismatch("Support must match length of weights."))
+	length(f.w) > 0 || return 0.0		# manage zero element
 	fx= 0.0
-	for j= 1:length(f.w)			# evaluate the kernel
+	for j= 1:length(f.w)				# evaluate the kernel at x
 		fx+= f.w[j]* f.kernel(x, f.x[j])
 	end
 	return fx/ length(f.w)
@@ -27,31 +29,26 @@ end
 
 #	│	outer constructors
 #	╰────────────────────────────────────────────────────
-function RKHSbyWeights(x::Vector{Float64}, weights, kernel= GaussKernel; fillGram= false)	#	function
+function RKHSbyWeights(x::Vector{T}, weights, kernel= GaussKernel; fillGram= false)	where T	#	function
 	if fillGram
 		RKHS(x, weights, Gram(x, kernel), kernel)
 	else
 		RKHS(x, weights, Array{Float64}(undef, 0, 0), kernel)
 end	end
 
-
 function RKHS(f0::RKHS; λ= 0.0, x= f0.x, kernel= f0.kernel)		#	λ-projection
 	GramM= Gram(x, kernel); n= length(f0.w)
 	RKHS(x, (λ*I*n+ GramM)\ (n* f0(x)), GramM, kernel)
 end
 
-function RKHS(x::Vector{Float64}, fx::Vector{Float64}; λ= 0.0, kernel= GaussKernel)	#	regression
+function RKHS(x::Vector{T}, fx::Vector{Float64}; λ= 0.0, kernel= GaussKernel)::RKHS{T} where T	#	regression
 	GramM= Gram(x, kernel); n= size(GramM,1)
 	RKHS(x, (λ*I*n+ GramM)\ (n* fx), GramM, kernel)
 end
 
-function RKHS(x::Array{Float64,2}; kernel= GaussKernel)	#	Gaussian random field
+function RKHS(x::Vector{T}; kernel= GaussKernel)::RKHS{T} where T	#	Gaussian random field
 	GramM= Gram(x, kernel)
-	new(x, rand(Distributions.MvNormalCanon( 0.0000001I+ GramM)), GramM, kernel)
-end
-
-function RKHS(x::Vector{Float64}; kernel= GaussKernel)	#	Gaussian random field
-	RKHS(reshape(x, :, 1); kernel= kernel)
+	RKHS(x, rand(Distributions.MvNormalCanon( 0.0000001I+ GramM)), GramM, kernel)
 end
 
 function RKHS(f0, nPoints::Int64, distD::UnivariateDistribution, kernel= GaussKernel; λ= 0.0)	# employ a distribution
@@ -86,16 +83,16 @@ function RKHSclean(f::RKHS{T}; fillGram= true)::RKHS{T} where T
 	if iszero(fw[ind[1]]); ind[1]= 0; end
 	for i = 2:length(fw)				# scan for duplicates and removeables
 		if ind[i-1] > 0 && f.x[ind[i]] == f.x[ind[i-1]]
-			fw[ind[i]]+= fw[ind[i-1]]	# roll forward the weitht of duplicate
+			fw[ind[i]]+= fw[ind[i-1]]	# roll forward the weight of duplicate
 			ind[i-1]= 0					# remove duplicate predecessor
 		end
 		if iszero(fw[ind[i]])			# weight 0 encountered
 			ind[i]= 0					# remove actual
 	end	end
-	filter!(!iszero, ind)				# remove now void elements
+	filter!(!iszero, ind)				# sweep out now all redundant elements
 	if fillGram
 		if size(f.GramMatrix) == (length(f.x),length(f.x))
-			for i in ind
+			for i in ind				# scan all entries
 				for j in ind
 					if isnan(f.GramMatrix[i,j])			# provide missing entry
 						if isnan(f.GramMatrix[j,i])		# employ symmetry
@@ -104,23 +101,23 @@ function RKHSclean(f::RKHS{T}; fillGram= true)::RKHS{T} where T
 							f.GramMatrix[i,j]= f.GramMatrix[j,i]
 			end	end	end	end
 			return RKHS(f.x[ind], fw[ind]* length(ind)/ length(f.w), Symmetric(f.GramMatrix[ind,ind]), f.kernel)
-		else
+		else							# compute from scratch
 			GramMatrix= Gram(f.x[ind], f.kernel)
 			return RKHS(f.x[ind], fw[ind]* length(ind)/ length(f.w), Symmetric(GramMatrix), f.kernel)
 		end
-	else
+	else	# don't fill the matrix
 		return RKHS(f.x[ind], fw[ind]* length(ind)/ length(f.w), Array{Float64}(undef, 0, 0), f.kernel)
 end	end
 
 #	│	- (minus) operation
 #	╰────────────────────────────────────────────────────
-function Base.:-(f1::RKHS, f2::RKHS)::RKHS		# minus operation
+function Base.:-(f1::RKHS{T}, f2::RKHS{T})::RKHS{T} where T		# minus operation
 	f1.kernel == f2.kernel || throw(ArgumentError("RKHS - (minus): the kernel functions differ"))
 	n1= length(f1.w); n2= length(f2.w)
 	if length(f1.GramMatrix) > 0 || length(f2.GramMatrix) > 0
 		GramM= fill(NaN, n1+n2, n1+n2)			# fill with NaN
-		if length(f1.GramMatrix)> 0; GramM[1:n1,1:n1]= f1.GramMatrix; end
-		if length(f2.GramMatrix)> 0; GramM[n1+1:n1+n2,n1+1:n1+n2]= f2.GramMatrix; end
+		if length(f1.GramMatrix)== n1; GramM[1:n1,1:n1]= f1.GramMatrix; end				# rescue old
+		if length(f2.GramMatrix)== n2; GramM[n1+1:n1+n2,n1+1:n1+n2]= f2.GramMatrix; end	# rescue old
 		return RKHS([f1.x;f2.x], [(n1+n2)*f1.w/n1; -(n1+n2)*f2.w/n2], GramM, f1.kernel)
 	else
 		return RKHS([f1.x;f2.x], [(n1+n2)*f1.w/n1; -(n1+n2)*f2.w/n2], Array{Float64}(undef,0,0), f1.kernel)
@@ -179,11 +176,13 @@ function GaussKernel(x, y; ℓ=1.)	#	Gauss kernel
 	return exp(-(norm(y-x)/ ℓ)^2)
 end
 
-function BrownKernel(x, y; ℓ=1.)	#	Wiener process
-	return min.(x,y)[1]
+function BrownianKernel(x, y; ℓ=1.)	#	Wiener process
+	HurstH= 0.5						#	fractional Brownian motion
+	return (x.^(2*HurstH)+ y.^(2*HurstH)- abs.(x.-y).^(2*HurstH))/2
+#	return min.(x,y)[1]
 end
 
-function BrownBridge(x, y; ℓ=1.)	#	Brownian bridge
+function BrownianBridge(x, y; ℓ=1.)	#	Brownian bridge
 	return min.(x,y)[1]- x[1]*y[1]
 end
 
